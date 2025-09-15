cd44b6248eb5c1d25aa93d7becfe0b612ab41f0d
Pushpasri M <127647687+pushpasri-M@users.noreply.github.com>
2025-09-15 15:15:26 +0530

    Add project files.

---
+++ Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Message
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hiiii");
+        }
+        public static void Waiting()
+        {
+            Console.WriteLine("Wait.....Man");
+        }
+
+    }
+}
