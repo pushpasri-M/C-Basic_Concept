8d602d4db436ae063f6c16f2aba02eafa3dc3679
Pushpasri M <mpushpasri2003@gmail.com>
2025-10-07 14:19:04 +0530

    Add

---
+++ LINQ/Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.LINQ
+{
+    class Data
+    {
+        public int canditID { get; set; }
+        public string name { get; set; }
+        public string designation { get; set; }
+        public int salary { get; set; }
+    }
+}
